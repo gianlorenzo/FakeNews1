7f591cada436070446da9dc6e25377f2990b88d1
gianlorenzo <gianlorenzo.didonato@gmail.com>
2018-03-13 14:24:04 +0100

    pulizia tweet ok

--- TakeTweet/TakeUsersTweets.py
+++ TakeTweet/TakeUsersTweets.py
@@ -4,42 +4,23 @@ import time
 
 from Tools import ElaboratoreTesto
 
+
 idUsers = DbConnection.takeUsersId()
 
 def writeFile():
-   start = time.time()
-   for id in idUsers:
+   for id in idUsers[0:1]:
        i = 0
        file = open(str(id)+".txt","w+")
        tweet = DbConnection.takeText(id)
        while(i<len(tweet)):
            text = tweet[i]['Tweet']['text']
-           newText = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
+           newText = unicodedata.normalize('NFKD', ElaboratoreTesto.pulisci(text)).encode('ascii', 'ignore')
            file.write(newText+"\n")
            i=i+1
        file.close()
-   end = time.time()
-   print(end - start)
-
-def writeFile1():
-   start = time.time()
-   for id in idUsers[0:1]:
-       i = 0
-       text1 = ""
-       file = open(str(id)+".txt","w+")
-       tweet = DbConnection.takeText(id)
-       while(i < 1):
-           text = tweet[i]['Tweet']['text']
-           testoRipulito = (ElaboratoreTesto.pulisci(text)
-           newText = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
-           file.write(newText+"\n")
-           i=i+1
-       file.close()
-   end = time.time()
-   print(end - start)
 
 
-writeFile1()
+writeFile()
 
 
 
--- Tools/ElaboratoreTesto.py
+++ Tools/ElaboratoreTesto.py
@@ -1,9 +1,8 @@
-#pip install nltk
-
-from nltk.tokenize import RegexpTokenizer
+from nltk import *
 import re
+
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+import stopwords
 
 #restituisce una lista di parole ripulite
 def pulisci(testo):
@@ -13,38 +12,26 @@ def pulisci(testo):
     #print(stopped)
     stemmed = stemmer(stopped)
     #print(stemmed)
-    for(text += stemmed[i] for i in stemmed)
-    return stemmed
+    return " ".join(stemmed)
 
 t = "Modernation !!! === ?!?! ,,,:;  Wheat Really Isn't Wheats 1000 At All https://t.co/Duy524oRNp ..."
 t2 = "https://t.co/4FK2CniCk3 : 819dfb7f-7d6b-4b70-847c-071eb489a53c"
 
-def tokenizer(testo):
+def tokenizer(text):
     #match solo parole di 3+ caratteri
     tokenizer = RegexpTokenizer('\w{3,}')
     #elimino i numeri e i links
-    result = re.sub(r"\d+","",re.sub(r"http\S+", "", testo))
+    result = re.sub(r"\d+","",re.sub(r"http\S+", "", text))
     return tokenizer.tokenize(result)
 
-def stopper(testo):
-    stop_words = set(stopwords.words('english'))
+def stopper(text):
+    stop_words = set(stopwords.get_stopwords('english'))
     print(stop_words)
-    #stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
-    result = [i.lower() for i in testo if i.lower() not in stop_words]
+    result = [i.lower() for i in text if i.lower() not in stop_words]
     return result
 
-def stemmer(testo):
+def stemmer(text):
     porter = PorterStemmer()
-    result = [porter.stem(i.lower()) for i in testo]
+    result = [porter.stem(i.lower()) for i in text]
     return result
-#print((type(stemmer(t))))
 
-print(pulisci(t))
-
-
-
-
-#stemmer di porter
-#stemmer di krovetz
-#POS tagging
-#n-grams
