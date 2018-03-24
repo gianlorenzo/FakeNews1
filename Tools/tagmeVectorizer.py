3c201bbc45b4ed2babd16b192d84f5e5ee457e7f
gianlorenzo <gianlorenzo.didonato@gmail.com>
2018-03-24 14:11:51 +0100

    inizio jaccart

--- Tools/tagmeVectorizer.py
+++ Tools/tagmeVectorizer.py
@@ -1,10 +1,6 @@
 import tagme
 from sklearn.metrics import jaccard_similarity_score
-import unicodedata
-import io
-from urllib2 import unquote
-
-import codecs
+import numpy as np
 
 # Authorization token
 tagme.GCUBE_TOKEN = "8a0bd46c-6638-493f-bd49-82e9965473ff-843339462"
@@ -23,10 +19,14 @@ def get_annotation(user_id):
     for ann in lunch_annotations.get_annotations(0.2):
         dictionary[ann.entity_title] = ann.score
     return dictionary
-#def get_jaccard_similarity(user_id1,user_id2):
-    #return jaccard_similarity_score(get_annotation(user_id1),get_jaccard_similarity(user_id2))
+def get_jaccard_similarity(user_id1,user_id2):
+    entity1 =[get_annotation(user_id1).keys()]
+    entity2 = [get_annotation(user_id2).keys()]
+    #entity1.append(get_annotation(user_id1).keys())
+    #entity2.append(get_annotation(user_id2).keys())
+    return jaccard_similarity_score(entity1,entity2)
 
-#print(get_jaccard_similarity(153692738,21032566))
-print get_annotation(153692738)
+print(get_jaccard_similarity(153692738,21032566))
+#print get_annotation(153692738)
 
 
