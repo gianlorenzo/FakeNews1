3cc2fe59e5d8c73af057f5d77740b6e865b52cbf
davben <Davide-blessed@hotmail.it>
2018-04-03 16:21:13 +0200

    cmm

--- Tools/tagmeVectorizer.py
+++ Tools/tagmeVectorizer.py
@@ -7,24 +7,27 @@ import operator
 # Authorization token
 tagme.GCUBE_TOKEN = "8a0bd46c-6638-493f-bd49-82e9965473ff-843339462"
 
-def take_text_by_id_of_cleandFile(user_id):
-    dir = "/home/gianlorenzo/PycharmProjects/FakeNews1/TakeTweet/CleanedFile/"+str(user_id)+".txt"
+dirNand = "/home/gianlorenzo/PycharmProjects/FakeNews1/"
+dirDav = "/home/davben/git/FakeNews1/"
+
+def take_text_by_id_of_cleanedFile(user_id,dirX):
+    dir = dirX+"TakeTweet/CleanedFile/"+str(user_id)+".txt"
     file = open(dir, "r+")
     user_text = file.read()
     return user_text
 
-def get_annotation_of_cleanedFile(user_id,score):
-    lunch_annotations = tagme.annotate(take_text_by_id_of_cleandFile(user_id))
+def get_annotation_of_cleanedFile(user_id,score,dirX):
+    lunch_annotations = tagme.annotate(take_text_by_id_of_cleanedFile(user_id,dirX))
     # Print annotations with a score higher than 0.2
     dictionary = {}
     for ann in lunch_annotations.get_annotations(score):
         dictionary[ann.entity_title] = ann.score
     return dictionary
 
-def writeFileCleanedAnnotation(user_id,score):
-    dict = get_annotation_of_cleanedFile(user_id,score)
+def writeFileCleanedAnnotation(user_id,score,dirX):
+    dict = get_annotation_of_cleanedFile(user_id,score,dirX)
     print(len(dict))
-    file = open("/home/gianlorenzo/PycharmProjects/FakeNews1/Tools/CleanedAnnotation/"+str(user_id)+".txt","w+")
+    file = open(dirX+"Tools/CleanedAnnotation/"+str(user_id)+".txt","w+")
     i = 0
     while(i<len(dict)):
         file.write(unicodedata.normalize('NFKD',dict.keys()[i]).encode('ascii', 'ignore')+"="+repr(dict.values()[i])+"\n")
@@ -32,24 +35,24 @@ def writeFileCleanedAnnotation(user_id,score):
     file.close()
 
 
-def take_text_by_id_of_noCleanedFile(user_id):
-    dir = "/home/gianlorenzo/PycharmProjects/FakeNews1/TakeTweet/NoCleanedFile/"+str(user_id)+".txt"
+def take_text_by_id_of_noCleanedFile(user_id,dirX):
+    dir = dirX+"TakeTweet/NoCleanedFile/"+str(user_id)+".txt"
     file = open(dir, "r+")
     user_text = file.read()
     return user_text
 
-def get_annotation_of_noCleanedFile(user_id,score):
-    lunch_annotations = tagme.annotate(take_text_by_id_of_noCleanedFile(user_id))
+def get_annotation_of_noCleanedFile(user_id,score,dirX):
+    lunch_annotations = tagme.annotate(take_text_by_id_of_noCleanedFile(user_id,dirX))
     # Print annotations with a score higher than 0.2
     dictionary = {}
     for ann in lunch_annotations.get_annotations(score):
         dictionary[ann.entity_title] = ann.score
     return dictionary
 
-def writeFileNoCleanedAnnotation(user_id,score):
-    dict = get_annotation_of_noCleanedFile(user_id,score)
+def writeFileNoCleanedAnnotation(user_id,score,dirX):
+    dict = get_annotation_of_noCleanedFile(user_id,score,dirX)
     print len(dict)
-    file = open("/home/gianlorenzo/PycharmProjects/FakeNews1/Tools/NoCleanedAnnotation/"+str(user_id)+".txt","w+")
+    file = open(dirX+"Tools/NoCleanedAnnotation/"+str(user_id)+".txt","w+")
     i = 0
     while(i<len(dict.keys())):
         file.write(unicodedata.normalize('NFKD',dict.keys()[i]).encode('ascii', 'ignore')+"="+repr(dict.values()[i])+"\n")
